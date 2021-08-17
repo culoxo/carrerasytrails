1758f5a90486ec56f9667b0ecd919c63e076d3d4
culoxo <culoxo@gmail.com>
2021-08-17 20:23:27 +0200

    17/08/2018

--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
     path('inicio/', views.index, name="index"),
     path('blog/', views.blog, name='blog'),
--- Proyecto/wsgi.py
+++ Proyecto/wsgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
+#path a donde esta el manage.py de nuestro proyecto Django
+sys.path.append('Users/David/EnMovimiento')
+
+#referencia (en python) desde el path anterior al fichero settings.py
+#Importante hacerlo así, si hay varias instancias coriendo (en lugar de setdefault)
+os.environ[‘DJANGO_SETTINGS_MODULE’] = “proyectodjango.settings”
+#os.environ.setdefault(“DJANGO_SETTINGS_MODULE”, “proyectodjango.settings”)
+
+#prevenimos UnicodeEncodeError
+os.environ.setdefault(“LANG”, “en_US.UTF-8”)
+os.environ.setdefault(“LC_ALL”, “en_US.UTF-8”)
+
+#activamos nuestro virtualenv
+activate_this = ‘pathToVirtualenv/bin/activate_this.py’
+execfile(activate_this, dict(__file__=activate_this))
+
+#obtenemos la aplicación
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto.settings')
-
 application = get_wsgi_application()
